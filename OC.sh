a12a9b17de9dd1d55b1b9cbeaacaa20a670800f5
Mikhail Chereshnev <Venuttv@yandex.ru>
2021-09-02 01:49:39 +0300

    Update OC.sh
    #! /usr/bin/env bash
    
    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
    apt-cache policy docker-ce
    
    apt update
    apt install -y qemu-kvm libvirt-daemon libvirt-daemon-system network-manager docker-ce docker-compose
    apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
    apt install -y cockpit

--- OC.sh
+++ OC.sh
@@ -1,1 +1,10 @@
+#! /usr/bin/env bash
 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-cache policy docker-ce
+
+apt update
+apt install -y qemu-kvm libvirt-daemon libvirt-daemon-system network-manager docker-ce docker-compose
+apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt install -y cockpit 
