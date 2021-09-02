5339167a54d93e66f9217623b2d0e22830f40656
Mikhail Chereshnev <Venuttv@yandex.ru>
2021-09-02 14:13:39 +0300

    Update OC.sh

--- OC.sh
+++ OC.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 echo 'Hello' > txt
-#sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-#sudo echo \
-#  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt update 
-#sudo apt-cache policy docker-ce
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update 
+sudo apt-cache policy docker-ce
 
 
 
-#sudo apt install -y qemu-kvm libvirt-daemon libvirt-daemon-system network-manager docker-ce docker-compose docker-ce-cli containerd.io
-#sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-#sudo apt install -y cockpit 
+sudo apt install -y qemu-kvm libvirt-daemon libvirt-daemon-system network-manager docker-ce docker-compose docker-ce-cli containerd.io
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt install -y cockpit 
